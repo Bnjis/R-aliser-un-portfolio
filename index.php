85dea3b46747c04fa9502ea2fd37f6ae4091155b
Benji <Benjamin.lienart@gmail.com>
2019-06-03 16:09:30 +0200

    Initialize

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <div class="homeSlider-imagesContainer">
         <div class="homeSlider-item" style="background-image:url(medias/01.jpg);"></div>
     </div>
-    <h1><span>Mon</span><span>Super</span><span>PortFolio</span></h1>
+    <h1><span class="-appear">Mon</span><span class="-appear">Super</span><span class="-appear">PortFolio</span></h1>
 </section>
 
 <?php
