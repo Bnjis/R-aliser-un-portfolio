abfce79c01f340fdf179202dd12c575d9a7f3943
Benji <Benjamin.lienart@gmail.com>
2019-06-03 09:41:50 +0200

    Initialize chap

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
     include 'header.php';
 ?>
 
-    HELLO WORLD
+<ul class="home--menu">
+    <li class="-appear"><a href="/archive.php" data-link="transition">Works</a></li>
+</ul>
+
+<section class="home--slider">
+    <div class="images">
+        <img src="medias/01.jpg" alt="">
+    </div>
+    <h2><span class="-appear">Jean</span><span class="-appear">Christophe</span><span class="-appear">Portugal</span></h2>
+</section>
 
 <?php
     include 'footer.php';
